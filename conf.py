834880e985ba266650599aa45c86876bf00d33f5
Weichao Qiu <qiuwch@gmail.com>
2016-11-08 02:54:07 -0500

    Not sure which conf file will fire.

---
+++ conf.py
@@ -0,0 +1,10 @@
+import subprocess, os
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+   # subprocess.call('cd ../doxygen; doxygen', shell=True)
+   print 'Test build'
+   with open('build.html', 'w') as f:
+       f.write('This is a test.')
+
